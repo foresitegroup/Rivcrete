5165d32902c2545d729b0baf3ca01e4e4a8ea718
foresitegroup <mark@foresitegrp.com>
2019-03-26 12:32:21 -0500

    Add portfolio page

--- header.php
+++ header.php
@@ -51,8 +51,9 @@ $FooterImage = (isset($FooterImage)) ? $FooterImage : "";
         <label for="show-menu" id="menu-toggle"></label>
         <div id="menu">
           <ul>
-            <li><a href="products-and-services.php">Products &amp; Services</a></li>
+            <li><a href="products-and-services.php">Products</a></li>
             <li><a href="decorative-products.php">Decorative</a></li>
+            <li><a href="portfolio.php">Portfolio</a></li>
             <li><a href="company.php">Company</a></li>
             <li><a href="contact.php">Contact</a></li>
             <li class="phone">(414) 455-6070</li>
---
+++ portfolio.php
@@ -0,0 +1,85 @@
+<?php
+$PageTitle = "Portfolio";
+include "header.php";
+?>
+
+<div class="page-header noimage">
+  <div class="site-width">
+    <div class="text">
+      <h1>Riv/Crete Project Portfolio</h1>
+      Riv/Crete strives to bring the highest quality work to every job. See some of our favorites below.
+
+      <h2>PROJECT GALLERIES</h2>
+    </div>
+
+    <div class="image"></div>
+  </div>
+</div>
+
+<div class="portfolios">
+  <div class="portfolio">
+    <div class="port-title">
+      <h3>Ixonia Bank</h3>
+      <h4>South Eastern Wisconsin Design Award Winner</h4>
+    </div>
+
+    <div class="port-text">
+      <div class="text">
+        Ixonia Bank wanted to showcase its new building with beautiful walkways and paths that were customer friendly and durable to tough Wisconsin winters.  The results were stunning and it was recognized by the Wisconsin Ready Mix Concrete Association as one of the top projects in all South Eastern Wisconsin.
+      </div>
+    </div>
+
+    <div class="port-image cycle-slideshow" data-cycle-timeout="0" data-cycle-slides="> div" data-cycle-pager-template="<a href=#>0{{slideNum}}</a>">
+      <div style="background-image: url(images/portfolio-ixonia-bank1.jpg);"></div>
+
+      <span class="cycle-pager"></span>
+    </div>
+  </div>
+
+  <div class="portfolio">
+    <div class="port-title">
+      <h3>Walkers Landing</h3>
+      <h4>ICF Project Design Award Winner</h4>
+    </div>
+
+    <div class="port-text">
+      <div class="text">
+        Walkers Landing which sits along the Milwaukee River was featured by the National Ready Mix Concrete Association as one of the top projects in the United States.  It was also recognized as the top ICF project in 2018 by the Wisconsin Ready Mix Concrete Association.
+      </div>
+    </div>
+
+    <div class="port-image cycle-slideshow" data-cycle-timeout="0" data-cycle-slides="> div" data-cycle-pager-template="<a href=#>0{{slideNum}}</a>">
+      <div style="background-image: url(images/portfolio-walkers-landing1.jpg);"></div>
+      <div style="background-image: url(images/portfolio-walkers-landing2.jpg);"></div>
+
+      <span class="cycle-pager"></span>
+    </div>
+  </div>
+
+  <div class="portfolio">
+    <div class="port-title">
+      <h3>Otter Exhibit - MKE Zoo</h3>
+      <h4>Commercial Decorative Design Award Winner</h4>
+    </div>
+
+    <div class="port-text">
+      <div class="text">
+        In early 2018 the Milwaukee County Zoo welcomed its newest members with the grand opening of the Otter Exhibit.  This incredible exhibit won an exclusive award from the Wisconsin Ready Mix Concrete Association for the Decorative Commercial category.
+      </div>
+    </div>
+
+    <div class="port-image cycle-slideshow" data-cycle-timeout="0" data-cycle-slides="> div" data-cycle-pager-template="<a href=#>0{{slideNum}}</a>">
+      <div style="background-image: url(images/portfolio-otter-exhibit1.jpg);"></div>
+      <div style="background-image: url(images/portfolio-otter-exhibit2.jpg);"></div>
+      <div style="background-image: url(images/portfolio-otter-exhibit3.jpg);"></div>
+
+      <span class="cycle-pager"></span>
+    </div>
+  </div>
+</div>
+
+<div class="order-band">
+  <a href="contact.php">ORDER CONCRETE</a>
+</div>
+
+<?php include "footer.php"; ?>
