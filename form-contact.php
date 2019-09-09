e0aedfdba915af5274ddb298713df1042112f352
foresitegroup <mark@foresitegrp.com>
2019-09-09 09:56:44 -0500

    Change BCC address

--- form-contact.php
+++ form-contact.php
@@ -68,7 +68,7 @@ if ($_POST['confirmationCAP'] == "") {
     $SendTo = "contactus@rivcrete.com";
     $Headers = "From: Contact Form <contactform@rivcrete.com>\r\n";
     $Headers .= "Reply-To: " . $_POST[md5('email' . $_POST['ip'] . $salt . $_POST['timestamp'])] . "\r\n";
-    $Headers .= "Bcc: mark@foresitegrp.com\r\n";
+    $Headers .= "Bcc: foresitegroupllc@gmail.com\r\n";
 
     $Message = $_POST[md5('name' . $_POST['ip'] . $salt . $_POST['timestamp'])] . "\n";
     if ($_POST[md5('company' . $_POST['ip'] . $salt . $_POST['timestamp'])] != "")
