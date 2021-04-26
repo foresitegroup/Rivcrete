01539ae70a56c553c0c4a6a12244fcce03d4eb9b
marklippert <lippert@gmail.com>
2021-04-26 14:40:49 -0500

    Add to portfolio page

--- portfolio.php
+++ portfolio.php
@@ -19,6 +19,65 @@ include "header.php";
 <div class="portfolios">
   <div class="portfolio">
     <div class="port-title">
+      <h3>Froedtert Parking</h3>
+    </div>
+
+    <div class="port-text">
+      <div class="text">
+        This five-story cast-in-place concrete structure is now one of the largest freestanding parking structures in the state of Wisconsin. In total, over 3300 parking spaces were added, an incredible 5.6 million tons of steel installed, and over 52,000 cubic yards of concrete poured. The new structure is one of the largest construction projects ever undertaken on the Froedtert campus.
+      </div>
+    </div>
+
+    <div class="port-image cycle-slideshow" data-cycle-timeout="0" data-cycle-slides="> div" data-cycle-pager-template="<a href=#>0{{slideNum}}</a>">
+      <div style="background-image: url(images/portfolio-froedtert-parking1.jpg);"></div>
+
+      <span class="cycle-pager"></span>
+    </div>
+  </div> <!-- /.portfolio -->
+
+  <div class="portfolio">
+    <div class="port-title">
+      <h3>R1ver Development</h3>
+    </div>
+
+    <div class="port-text">
+      <div class="text">
+        The R1ver development sits on a 6-acre site flanked on two sides by the Milwaukee River. Standing tall is the 8-story corporate office building that will be home to Michels Corporation. Additionally, a new apartment building and future hotel make up this incredible development.
+      </div>
+    </div>
+
+    <div class="port-image cycle-slideshow" data-cycle-timeout="0" data-cycle-slides="> div" data-cycle-pager-template="<a href=#>0{{slideNum}}</a>">
+      <div style="background-image: url(images/portfolio-r1ver1.jpg);"></div>
+
+      <span class="cycle-pager"></span>
+    </div>
+  </div> <!-- /.portfolio -->
+
+  <div class="portfolio">
+    <div class="port-title">
+      <h3>Bass Bay Brewhouse</h3>
+    </div>
+
+    <div class="port-text">
+      <div class="text">
+        Sitting along the shores of Bass Lake sits the newly renovated Bass Bay Brewhouse. The outdoor space was recently updated to create an outdoor dining and event space to accommodate growing customer demand. Winning a design award in this category is extremely difficult and Nord Construction delivered with incredible results.
+      </div>
+    </div>
+
+    <div class="port-image cycle-slideshow" data-cycle-timeout="0" data-cycle-slides="> div" data-cycle-pager-template="<a href=#>0{{slideNum}}</a>">
+      <div style="background-image: url(images/portfolio-bass-bay-brewhouse1.jpg);"></div>
+
+      <span class="cycle-pager"></span>
+    </div>
+  </div> <!-- /.portfolio -->
+
+
+
+
+
+
+  <div class="portfolio">
+    <div class="port-title">
       <h3>Saint John's On The Lake</h3>
       <h4>Education, Healthcare & Public Design Award Winner</h4>
     </div>
@@ -35,7 +94,7 @@ include "header.php";
 
       <span class="cycle-pager"></span>
     </div>
-  </div>
+  </div> <!-- /.portfolio -->
 
   <div class="portfolio">
     <div class="port-title">
@@ -55,13 +114,7 @@ include "header.php";
 
       <span class="cycle-pager"></span>
     </div>
-  </div>
-
-
-
-
-
-
+  </div> <!-- /.portfolio -->
 
   <div class="portfolio">
     <div class="port-title">
@@ -80,7 +133,7 @@ include "header.php";
 
       <span class="cycle-pager"></span>
     </div>
-  </div>
+  </div> <!-- /.portfolio -->
 
   <div class="portfolio">
     <div class="port-title">
@@ -100,7 +153,7 @@ include "header.php";
 
       <span class="cycle-pager"></span>
     </div>
-  </div>
+  </div> <!-- /.portfolio -->
 
   <div class="portfolio">
     <div class="port-title">
@@ -121,8 +174,8 @@ include "header.php";
 
       <span class="cycle-pager"></span>
     </div>
-  </div>
-</div>
+  </div> <!-- /.portfolio -->
+</div> <!-- /.portfolios -->
 
 <div class="order-band">
   <a href="contact.php">ORDER CONCRETE</a>
