05daddaf3cdb6260f321409329176edbef0e6493
foresitegroup <mark@foresitegrp.com>
2022-01-10 16:01:37 -0600

    Add Environmental page

---
+++ environmental.php
@@ -0,0 +1,66 @@
+<?php
+$PageTitle = "Environmental";
+include "header.php";
+$FooterImages = "no";
+?>
+
+<div class="page-header noimage">
+  <div class="site-width">
+    <div class="text">
+      <h1>Riv/Crete Environmental</h1>
+    </div>
+
+    <div class="image"></div>
+  </div>
+</div>
+
+<div class="site-width environmental">
+  <img src="images/environmental1.jpg" alt="" class="alignright">
+  Riv/Crete Ready Mix is a family-owned business based in Milwaukee, Wisconsin.  Our company is focused on providing our customers with the best service and highest quality concrete in the Milwaukee market.  Along with that focus we also believe our company has a responsibility is to minimize our environmental impact as best we can. Here are just a few of the impacts we are making.
+
+  <div style="clear: both;"></div>
+  <br>
+
+  <h2>Carbon Cure</h2>
+  <div class="col2">
+    <p>Concrete is the most used man-made material on earth and is second only to consumption of water.  Concrete itself has a minimal impact on the environment.  However, the main ingredient used to make concrete is called cement which emits Co2 during its production process.  Roughly every ton of cement that is produced releases .90 tons of carbon into the atmosphere.  The cement industry is undergoing a major transition in cement production reducing their emissions by almost 10%.   Coinciding with this change is the introduction of a cutting-edge technology called Carbon Cure.</p>
+
+    <p>Cabon Cure technology injects captured Co2 into concrete during the mixing process.  A chemical reaction occurs converting the gas back into particle size grains of limestone.  This chemical reaction provides additional strength to our concrete mixes allowing us to use less cement in the manufacturing process and still maintain our strength requirements.  When the chemical reaction occurs it permanently locks captured carbon dioxide in concrete.  Even if the concrete is demolished, mineralized Co2 will never leak or return to the atmosphere.</p>
+  </div>
+
+  <div id="carboncure"><embed src="https://mycarboncureapi.com/widgets?widgetName=embeddedTicker&version=1&name=Rivcrete&id=2202&timeframe=All Time&units=Imperial&production=Volume&environmental=Forestland"/></div>
+  <br>
+
+  <h3>Concrete Waste</h3>
+  We have zero waste in our concrete production because we use or re-use 100% of the material we produce.    Any leftover concrete not used by our customers is returned to the concrete plant where it was originally manufactured and used to make concrete block or become road base. We have not landfilled returned concrete in over a decade.<br>
+  <br>
+
+  <div class="cw">
+    <img src="images/concrete-block.jpg" alt="">
+
+    <div>
+      <h4>Concrete Block</h4>
+      Returned concrete is poured into metal forms to become concrete bin block.  These 2x2x4 bin block interlock and stack to form large storage areas that are ideal for landscapers, salt storage, or even concrete companies.
+    </div>
+  </div> <!-- /.cw -->
+
+  <div class="cw">
+    <img src="images/road-base.jpg" alt="">
+
+    <div>
+      <h4>Road Base</h4>
+      Returned concrete that is not used to make concrete block is dumped into large piles that will eventually be crushed and converted into road base.  This material is used to establish a durable surface for eventual roads and streets that our vehicles travel on.
+    </div>
+  </div> <!-- /.cw -->
+
+  <div class="cw">
+    <img src="images/clean-burn.jpg" alt="">
+
+    <div>
+      <h4>Clean Burn</h4>
+      Concrete trucks require frequent oil changes to run efficiently and help prevent mechanical breakdowns.  The amount of oil required to maintain our fleet is significant and burdensome to store and dispose.  To address this issue Riv/Crete invested in a system called Clean Burn.  The Clean Burn system uses "old" oil as its fuel source for heating our mechanic's garage.  Throughout the year we store the used oil in an environmentally safe container.  During the winter months we then tap into this fuel source to keep our mechanics warm while recycling the "old" oil as our fuel source.
+    </div>
+  </div> <!-- /.cw -->
+</div> <!-- .environmental -->
+
+<?php include "footer.php"; ?>
--- header.php
+++ header.php
@@ -77,6 +77,7 @@ $FooterImage = (isset($FooterImage)) ? $FooterImage : "";
               <a href="<?php echo $TopDir; ?>company.php">Company</a>
               <ul>
                 <li><a href="<?php echo $TopDir; ?>blog">News</a></li>
+                <li><a href="<?php echo $TopDir; ?>environmental.php">Environmental</a></li>
                 <li><a href="<?php echo $TopDir; ?>resources.php">Resources</a></li>
               </ul>
             </li>
