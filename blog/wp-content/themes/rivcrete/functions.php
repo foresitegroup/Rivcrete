8249a6399bd63146a21086df67a9cfca05b737ef
Mark Lippert <mark@foresitegrp.com>
2022-09-27 12:20:55 -0500

    Remove email notification about automatic updates

--- blog/wp-content/themes/rivcrete/functions.php
+++ blog/wp-content/themes/rivcrete/functions.php
@@ -1,4 +1,8 @@
 <?php
+// I don't need to be notified about every automatic update
+add_filter( 'auto_plugin_update_send_email', '__return_false' );
+add_filter( 'auto_theme_update_send_email', '__return_false' );
+
 // We want Featured Images on Pages and Posts
 add_theme_support( 'post-thumbnails' );
 
